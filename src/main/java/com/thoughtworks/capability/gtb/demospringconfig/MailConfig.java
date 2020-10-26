b1e49e5174940ecbb1a9f3004b4c130b794f6e51
di.qin <di.qin@thoughtworks.com>
2020-10-26 18:26:31 +0800

    [qd]: finish problem1.

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,18 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String, Boolean> additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +39,43 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, Boolean> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map<String, Boolean> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients[0]=" + defaultRecipients.get(0) + '\'' +
+                ", defaultRecipients[1]=" + defaultRecipients.get(1) + '\'' +
+                ", additionalHeaders.redelivery=" + additionalHeaders.get("redelivery") + '\'' +
+                ", additionalHeaders.secure=" + additionalHeaders.get("secure") + '\'' +
+                ", credentials.username=" + credentials.getUsername() + '\'' +
+                ", credentials.password=" + credentials.getPassword() + '\'' +
+                ", credentials.authMethod=" + credentials.getAuthMethod() + '\'' +
                 '}';
     }
 }
